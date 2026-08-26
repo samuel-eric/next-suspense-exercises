6e17b4ae73e93300de6e6591671b9f20d602effe
samuel-eric <samueltan086@gmail.com>
2026-08-26 19:10:36 +0700

    finish exercise 2

---
+++ src/components/CommentList/CommentList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { getComments } from "@/helpers/interview-helpers";
+import Comment from "@/components/Comment";
+
+async function CommentList() {
+	const comments = await getComments();
+
+	return comments.map((comment) => (
+		<Comment key={comment.id} comment={comment} />
+	));
+}
+
+export default CommentList;
